45e60edbeb77e4bc872059cbff01e634f0a0bfcd
theaveasso <theaveas.so@gmail.com>
2022-05-21 00:11:17 +0800

    update v0.1.2

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,44 @@
-use std::io;
-use chrono;
-use colored::Colorize;
+use std::env;
 
+struct TDList {
+    items: Vec<TDItem>
+}
+impl TDList {
+    fn new() -> TDList {
+        TDList { items: Vec::new() }
+    }
+
+    fn append(&mut self, todo: String) {
+        let item = TDItem::new(todo);
+        self.items.push(item);
+    }
+
+    fn print(&self) {
+        for item in &self.items {
+            println!("[{}] - {}", item.done, item.todo)
+        }
+    }
+    
+}
 struct TDItem {
-    todo: String,
-    done: String
+    done: char,
+    todo: String
+}
+impl  TDItem {
+    fn new(todo: String) -> TDItem {
+        TDItem { done: ' ', todo }
+    }
+    
 }
 
 fn main() {
-    let write = String::from("write");
-    let read = String::from("read");
-    let quit = String::from("quit");
-    
+    let args: Vec<String> = env::args().collect();
+    let cmd = &args[1];
+    // let todo_item = TDItem::new("Make todo app with Rust🦀".to_string());
+    let mut todo_list = TDList::new();
+    todo_list.append("Make todo app with Rust🦀".to_string());
 
-    println!("🧾 todo ...");
-    println!("  L --{}  L --{}  L --{}", write.green(), read.yellow(), quit.red());
-
-    loop {
-        // get user input
-        let mut todo = String::new();
-        io::stdin()
-            .read_line(&mut todo)
-            .expect("Failed to read line!");
-        let todo = todo.trim();
-
-        if todo.eq(&quit) {
-            break;
-        } else if todo.eq(&write) {
-            write_list()
-        } else if todo.eq(&read) {
-            println!("test read");
-        } else {
-            println!("{}: ({}: to add, {}: to read, {}: to exit the program)", "hint".purple(), write.green(), read.yellow(), quit.red());
-            continue;
-        };
-    }
-}
-
-// write function
-// adding list to a Todo item
-fn td_append(s: String) -> TDItem {
-    TDItem { todo: s, done: String::from("[ ]") }
-}
-// collect user input than add to to do list
-fn write_list(){
-    let stop = String::from('n');
-
-    loop {
-        println!("      L --continue: {}", "[n]".red());
-        let mut done = String::new();
-        
-        // BUGS println!("    -Todo item:"); FIXED
-        io::stdin()
-           .read_line(&mut done)
-           .expect("Failed to read lines");
-        let done = done.trim();
-
-        // check if the user is done adding
-        if done.eq(&stop){
-            println!("  L --{}  L --{}  L --{}","write".green(), 
-                                                "read".yellow(), 
-                                                "quit".red());
-            break;
-        } else {
-            let todo = done.trim();
-            td_append(String::from(todo));
-        }
-       
+    if cmd == "get" {
+        todo_list.print()
     }
 }
